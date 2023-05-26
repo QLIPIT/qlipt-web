0b04a8b7a310d7235d559a90c895f45f4532932a
alabo-excel <iamalaboexcel@gmail.com>
2023-05-26 12:13:42 +0100

    connect wallet page commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { Fragment } from 'react'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
-      <title>Qlipt</title>
+      <title>Qlip</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <Component {...pageProps} />
     </Fragment>
--- src/pages/wallet-connect.tsx
+++ src/pages/wallet-connect.tsx
@@ -8,9 +8,34 @@ const Wallet = () => {
       <section className='lg:p-32 p-6'>
         <div className='mb-10'>
           <h1 className='heading lg:text-5xl text-3xl font-bold'>Connect your Wallet</h1>
-            <p className='my-3 text-[#BEC2CA]'>Connect with one of available wallet providers or create a new wallet. <br /> <a href="">What is a wallet?</a></p>
+          <p className='my-3 text-[#BEC2CA]'>Connect with one of available wallet providers or create a new wallet. <br /> <a href="">What is a wallet?</a></p>
         </div>
-
+        <div className='flex justify-between flex-wrap'>
+          <div className='lg:w-[31%] w-[48%] lg:p-10 p-4 lg:my-6 my-2 border border-[#16161E] text-center rounded-sm'>
+            <img src="/assets/wallets/connect.png" className='mx-auto sm:w-20 sm:h-20' alt="" />
+            <p className='font-bold lg:text-lg text-xs'>Wallet Connect</p>
+          </div>
+          <div className='lg:w-[31%] w-[48%] lg:p-10 p-4 lg:my-6 my-2 border border-[#16161E] text-center rounded-sm'>
+            <img src="/assets/wallets/metamask.png" className='mx-auto sm:w-20 sm:h-20' alt="" />
+            <p className='font-bold lg:text-lg text-xs'>MetaMask</p>
+          </div>
+          <div className='lg:w-[31%] w-[48%] lg:p-10 p-4 lg:my-6 my-2 border border-[#16161E] text-center rounded-sm'>
+            <img src="/assets/wallets/coinbase.png" className='mx-auto sm:w-20 sm:h-20' alt="" />
+            <p className='font-bold lg:text-lg text-xs'>Coinbase</p>
+          </div>
+          <div className='lg:w-[31%] w-[48%] lg:p-10 p-4 lg:my-6 my-2 border border-[#16161E] text-center rounded-sm'>
+            <img src="/assets/wallets/formatic.png" className='mx-auto sm:w-20 sm:h-20' alt="" />
+            <p className='font-bold lg:text-lg text-xs'>Formatic</p>
+          </div>
+          <div className='lg:w-[31%] w-[48%] lg:p-10 p-4 lg:my-6 my-2 border border-[#16161E] text-center rounded-sm'>
+            <img src="/assets/wallets/myeth.png" className='mx-auto sm:w-20 sm:h-20' alt="" />
+            <p className='font-bold lg:text-lg text-xs'>MyEther Wallet</p>
+          </div>
+          <div className='lg:w-[31%] w-[48%] lg:p-10 p-4  lg:my-6 my-2 border border-[#16161E] text-center rounded-sm'>
+            <img src="/assets/wallets/trustwallet.png" className='mx-auto sm:w-20 sm:h-20' alt="" />
+            <p className='font-bold lg:text-lg text-xs'>TrustWallet</p>
+          </div>
+        </div>
       </section>
       <NotificationFlag />
     </FrontLayout>
